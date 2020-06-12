67e1e99e14ffc5a71c8ff480a6b6a9aa143a104e
unknown <nicholasnobida@yahoo.com>
2020-06-12 10:23:51 -0700

    not finished

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const App = () => {
+const App = (props) => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [number, setNumber] = useState(1);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
+      <
       <h1 className="Header">Characters</h1>
     </div>
   );
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+
+function getCharacter(id) {
+    const [character, setCharacter] = useState('')
+        useEffect(() => {
+            const fetchCharacter = (id) => 
+            fetch(`https://swapi.dev/api/people/${id}/`)
+                .then(response => response.json())
+                .then(id => {
+                    console.log(id);
+                    setCharacter(id.name);
+                });
+            fetchCharacter(id);
+    }, [id])
+    return character;
+}
+
+function Character({ id }){
+    const character = getCharacter(id)
+    
+    return (
+        <div className = 'character'>
+            <div>{id}</div>
+        </div>
+    );
+}
